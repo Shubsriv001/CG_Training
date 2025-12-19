391c8e1b4b47409a9b81e20852617d0e73c03c4f
Shubhanshu <shubsriv001@gmail.com>
2025-12-19 19:15:43 +0530

    hello world message

---
+++ CG_Day1/Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //1)WAP to print the Hello World message
+        // Console.WriteLine("Hello World");
+
+        // ---------------------------------------------
+
+    }
+}
