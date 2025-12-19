d14307ac05addb86860c889fa4421855f89d5adb
Shubhanshu <shubsriv001@gmail.com>
2025-12-19 18:56:07 +0530

    Added Employee class

---
+++ DAY_1/Employee.cs
@@ -0,0 +1,48 @@
+using System;
+namespace DAY_1;
+
+    class Employee
+{
+    int Id;
+    string Name;
+    string Dept;
+    float Salary;
+    string Gender;
+
+
+    public void AcceptDetails()
+    {
+        Console.WriteLine("Enter Employee Id: ");
+        Id=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter Employee name: ");
+        Name=Console.ReadLine();
+
+        Console.WriteLine("Enter Employee Dept: ");
+        Dept=Console.ReadLine();
+
+        Console.WriteLine("Enter Employee salary: ");
+        Salary=Convert.ToSingle(Console.ReadLine());
+
+        Console.WriteLine("Enter Employee gender: ");
+        Gender=Console.ReadLine();
+
+    }
+    public void display()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Employee Data  Are: ");
+
+        // This is one of the way to print the value using String interpolation
+         // Console.WriteLine($"Employee Name: {Name}");
+
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee Dept: " + Dept);
+        Console.WriteLine("Employee Salary: " + Salary);
+        Console.WriteLine("Employee Gender: " + Gender);
+        
+    }
+
+}
+
+
--- DAY_1/Program.cs
+++ DAY_1/Program.cs
@@ -4,11 +4,32 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Calculator calculator_obj=new Calculator();
-        calculator_obj.add();
-        calculator_obj.sub();
-        calculator_obj.mul();
-        calculator_obj.div();
-        calculator_obj.Remainder();
+
+        // Below is the Object For Calculaor class
+        // ---------------------------------------------
+
+        // Calculator calculator_obj=new Calculator();
+        // calculator_obj.add();
+        // calculator_obj.sub();
+        // calculator_obj.mul();
+        // calculator_obj.div();
+        // calculator_obj.Remainder();
+
+        // -----------------------------------------------
+
+        // Below is the Object For Employee class
+        // ---------------------------------------------
+
+        Employee employee_obj= new Employee();
+        employee_obj.AcceptDetails();
+        employee_obj.display();
+
+        // ---------------------------------------------
+
+
+
+
+
+
     }
 }
