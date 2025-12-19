50fe0756673283f56d58a3b4ad4ad54963f28f91
Shubhanshu <shubsriv001@gmail.com>
2025-12-19 19:22:20 +0530

    Print back user floating point number

--- CG_Day1/Programs/Program.cs
+++ CG_Day1/Programs/Program.cs
@@ -9,10 +9,21 @@ class Program
         // ---------------------------------------------
 
         //2)WAP to take number from the user and print it back
-            Console.WriteLine("Enter the number to print back:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"number enterde by the user is :{num}");
-            
+            // Console.WriteLine("Enter the number to print back:");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine($"number enterde by the user is :{num}");
+
         // --------------------------------------------------
+
+
+        //3) WAP to read floating point number from the user
+
+            Console.WriteLine("Enter floating point number");
+
+            float float_number = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"floating number entered by the user is {float_number}");
+
+        //---------------------------------------------------------------------------------------------
     }
 }
