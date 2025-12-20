731405a706ce2a04ebe33df8cb09bfbc1f3ac928
Shubhanshu <shubsriv001@gmail.com>
2025-12-20 12:04:38 +0530

    Added code of foreach loop for finding the sum of the passed arguments

--- CG_DAY3/apps/Function.cs
+++ CG_DAY3/apps/Function.cs
@@ -3,6 +3,8 @@ using System;
 class function
 {
 
+// --------------------------------------------------------------------------------------------
+
     // Name Argument:- we can change the order while passing the value to the function
 
     // public void Person(string name, int age, string city)
@@ -10,6 +12,7 @@ class function
     //     Console.WriteLine($"Name {name} Age {age} and city {city}");
     // }
 
+// ---------------------------------------------------------------------------------------------
 
     // Default Parameter
 
@@ -19,16 +22,20 @@ class function
     //     Console.WriteLine($"Name {name} Age {age} and city {city}");
 
     // }
+    
+    // Use of param-> use to take multiple input as argument while calling the function
 
+            public void add_num(params int[] nums)
+    {
 
-
-
-        // Use of param-> use to take multiple input as argument while calling the function
-
-    //         public void weeks(params string[] week)
-    // {
-    //     Console.WriteLine(string.Join(" ",week));
-    // }
+        int sum=0;
+        foreach(int n in nums)
+        {
+            sum+=n;
+        }
+        // Console.WriteLine(string.Join(" ",week));
+        Console.WriteLine(sum);
+    }
         
 
     public static void Main(string[] args)
@@ -36,18 +43,25 @@ class function
         //creating object on the class
         function fun_obj=new function();
 
+// --------------------------------------------------------------------------------------------
         // Named Argument Object:
+
         // fun_obj.Person(age:21,name:"shubhanshu", city:"varanasi");
+
+// --------------------------------------------------------------------------------------------
 
         // Default Parameter:
 
         // fun_obj.Person(age:21,name:"shubhanshu", city:"varanasi");
         // fun_obj.Person(age:21,name:"hjk");
 
+// ---------------------------------------------------------------------------------------------
+
         // below is the example of paran argument
         // fun_obj.weeks("sunday","monday");
 
 
+// ------------------------------------------------------------------------------------------------
     // BELOW is the Example of foreach Loop Accessing all the element of the array.
 
     // int []arr={1,2,3,4,5,6,7,8,9,10};
@@ -56,15 +70,19 @@ class function
     //         Console.WriteLine(a);
     //     }
 
-    string Name="Shubhanshu";
-    foreach(char a in Name)
-        {
-            Console.WriteLine(a);
-        }
+    // string Name="Shubhanshu";
+    // foreach(char a in Name)
+    //     {
+    //         Console.WriteLine(a);
+    //     }
+// ----------------------------------------------------------------------------------------------
+ 
+//  Calling the param function using object of the class and passing multiple value 
+
+    fun_obj.add_num(1,2,3,4,5,6,7,8,9,10);
 
 
-
-
+// -----------------------------------------------------------------------------------------------
 
     }
 }
--- CG_DAY3/apps/obj/Debug/net9.0/apps.AssemblyInfo.cs
+++ CG_DAY3/apps/obj/Debug/net9.0/apps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f04cf0c7a6a6e7eaf59d68f19e60d545481c66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de18c5025a85153915ae024c204fdca819ed6d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("apps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
