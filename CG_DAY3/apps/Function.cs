3cd612376a277ab78c7be80f1ee202c268440f77
Shubhanshu <shubsriv001@gmail.com>
2025-12-20 12:49:50 +0530

    Added the code to pass array as a argument to the params

--- CG_DAY3/apps/Function.cs
+++ CG_DAY3/apps/Function.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 class function
 {
@@ -22,28 +24,80 @@ class function
     //     Console.WriteLine($"Name {name} Age {age} and city {city}");
 
     // }
+
+// ---------------------------------------------------------------------------------------------
     
     // Use of param-> use to take multiple input as argument while calling the function
 
-            public void add_num(params int[] nums)
+    // param should be always at the last
+     public void add_num(params int[] nums)
     {
-
         int sum=0;
         foreach(int n in nums)
         {
             sum+=n;
         }
+
         // Console.WriteLine(string.Join(" ",week));
         Console.WriteLine(sum);
+        Console.WriteLine(a);
     }
         
 
+// ----------------------------------------------------------------------------
+//-----------------------------Pass By value and Pass by Value--------------------------
+
+// 1) Pass By Reference:- When we want to change the value parmanently.
+
+// public void increase_ten(ref int a)
+//         {
+//             a+=10;
+//         }
+
+// ----------------------------------------------------------------------------------------------
+// Example of out this is use to store the value and return it
+
+// 1) Example
+
+    // public static void Divide(int a, int b, out int quotient, out int remainder)
+    // {
+    //     quotient = a / b;
+    //     remainder = a % b;
+    // }
+
+// ----------------------------------------------------------------------------------------------
+
+// 2) Example
+
+//   public static void GetResult(int marks, out string grade)
+//     {
+//         if (marks >= 60)
+//             grade = "Pass";
+//         else
+//             grade = "Fail";
+//     }
+
+
+// ----------------------------------------------------------------------------------------------
+
+// In Methods
+// static void show(in int x)
+// {
+//     Console.WriteLine(x);
+// }
+
+
+
+
+
+// ----------------------------------------------------------------------------------------------
     public static void Main(string[] args)
     {
         //creating object on the class
         function fun_obj=new function();
 
 // --------------------------------------------------------------------------------------------
+
         // Named Argument Object:
 
         // fun_obj.Person(age:21,name:"shubhanshu", city:"varanasi");
@@ -62,6 +116,7 @@ class function
 
 
 // ------------------------------------------------------------------------------------------------
+
     // BELOW is the Example of foreach Loop Accessing all the element of the array.
 
     // int []arr={1,2,3,4,5,6,7,8,9,10};
@@ -79,10 +134,54 @@ class function
  
 //  Calling the param function using object of the class and passing multiple value 
 
-    fun_obj.add_num(1,2,3,4,5,6,7,8,9,10);
+    // fun_obj.add_num(1,2,3,4,5,6,7,8,9,10);
+
+    // If we want to pass the array to the param we need to pass it as a single array variable
+    int []brr={1,2,3,4,5,6,7,8,9,10};
+
+    fun_obj.add_num(brr);
 
 
-// -----------------------------------------------------------------------------------------------
 
+
+// ----------------------------------------------------------------------------------------------
+
+
+
+// 1) Pass By Value function calling
+
+// int x=10;
+// fun_obj.increase_ten(ref x);
+// Console.WriteLine(x);
+
+// --------------------------------------------------------------------------------------------
+// ---------------------------calling out function----------------------------------- 
+
+// 1) Example
+
+        // int q, r; 
+
+        // function.Divide(10, 3, out q, out r);
+
+        // Console.WriteLine("Quotient = " + q);
+        // Console.WriteLine("Remainder = " + r);
+
+// --------------------------------------------------------------------------------------------
+
+// 2) Example
+
+//   string result;
+//         function.GetResult(75, out result);
+//         Console.WriteLine(result);
+
+// --------------------------------------------------------------------------------------------
+
+
+// -----------------------------------------In Method Calling---------------------------------
+
+
+// int num = 50;
+// function.show(num);
+// --------------------------------------------------------------------------------------------
     }
 }
--- CG_DAY3/apps/obj/Debug/net9.0/apps.AssemblyInfo.cs
+++ CG_DAY3/apps/obj/Debug/net9.0/apps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de18c5025a85153915ae024c204fdca819ed6d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731405a706ce2a04ebe33df8cb09bfbc1f3ac928")]
 [assembly: System.Reflection.AssemblyProductAttribute("apps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
