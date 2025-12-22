318af0b3059c7921956dd6390dd33a219197780a
Shubhanshu <shubsriv001@gmail.com>
2025-12-22 12:44:01 +0530

    Added the  code of overloaded indexer for library management system

---
+++ CG_DAY4/Library Management System/library_management_system/Library.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Library
+{
+   private Dictionary<int, string> book = new Dictionary<int, string>();
+
+
+
+     public string this[int id]
+    {
+        get { return book[id]; }
+        set { book[id] = value; }
+    }
+
+    public string this[string name]
+    {
+        get
+        {
+            return book.FirstOrDefault(e => e.Value == name).Value;
+        }
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Library library_obj=new Library();
+        library_obj[0]="Harry Potter";
+        library_obj[1]="Merchant of venice";
+        library_obj[2]="The Tempest";
+
+        Console.WriteLine("Books are access by using the Index: ");
+
+        Console.WriteLine($"Name of the 1st Book is: {library_obj[0]}");
+        Console.WriteLine($"Name of the 2nd Book is: {library_obj[1]}");
+        Console.WriteLine($"Name of the 3rd Book is: {library_obj[2]}");
+
+
+        Console.WriteLine("");
+        Console.WriteLine("Books are access by using the name of the book: ");
+
+
+        Console.WriteLine($"Name of the 1st Book is: {library_obj["Harry Potter"]}");
+        Console.WriteLine($"Name of the 2nd Book is: {library_obj["Merchant of venice"]}");
+        Console.WriteLine($"Name of the 3rd Book is: {library_obj["The Tempest"]}");
+    }
+}
