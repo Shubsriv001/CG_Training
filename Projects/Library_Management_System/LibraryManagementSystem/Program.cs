24d667a99896189164c3661acf13148e61d0e470
Shubhanshu <shubsriv001@gmail.com>
2025-12-23 14:38:43 +0530

    Implementaed Interface

--- Projects/Library_Management_System/LibraryManagementSystem/Program.cs
+++ Projects/Library_Management_System/LibraryManagementSystem/Program.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using LibraryManagementSystem;
-class Program
-{
-    public static void Main(string[] args)
-    {
-         Book book_obj=new Book();
-         Magazine magazine_obj=new Magazine();
+﻿// using System;
+// using LibraryManagementSystem;
+// class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//          Book book_obj=new Book();
+//          Magazine magazine_obj=new Magazine();
 
-         book_obj.calculate_late_fee();
-         book_obj.display_item_details();
+//          book_obj.calculate_late_fee();
+//          book_obj.display_item_details();
 
-         magazine_obj.calculate_late_fee();
-         magazine_obj.display_item_details();
+//          magazine_obj.calculate_late_fee();
+//          magazine_obj.display_item_details();
 
-    }
-}
+//     }
+// }
