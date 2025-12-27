6d143dcb497bc09dfd7f2d2206aeaa0cd2c82a9a
Shubhanshu <shubsriv001@gmail.com>
2025-12-27 14:10:15 +0530

    Added the code for MediSure Client

---
+++ CG_Day8/MediSureClinic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Medical;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("================== MediSure Clinic Billing ==================");
+
+        PtientBill pb = new PtientBill();
+        bool run=true;
+
+        while (run)
+        {
+            Console.WriteLine("Enter your option:");
+            Console.WriteLine("1) Create New Bill");
+            Console.WriteLine("2) View Last Bill");
+            Console.WriteLine("3) Clear Last Bill");
+            Console.WriteLine("4) Exit");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    pb.CreateNewBill();
+                    break;
+
+                case 2:
+                    pb.ViewLastBill();
+                    break;
+
+                case 3:
+                    pb.ClearLastBill();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thank you. Application closed normally.");
+                        run=false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Selection. Please try again.");
+                    break;
+            }
+        }
+    }
+}
