a0a7d46972b1893381afb1de7f5a7b34511ef88d
Shubhanshu <shubsriv001@gmail.com>
2026-01-02 14:28:03 +0530

    added the code of Idisposable

--- CG_Day11/Linq/Linq.cs
+++ CG_Day11/Linq/Linq.cs
@@ -1,21 +1,110 @@
+// // present in the 'namespace System.Linq'
+// //  the linq won't work until we started accesing the value.. aslo known as deffered Execution // 
+//  class Linq 
+//  {  
+    
+
+//     // This is the example Sorting the Salary in the Ascending order....
+// //     class Emplyoee
+// // {
+// //     public string Name{get;set;}
+// //     public int Salary{get;set;}
+// // }
+
+
+
+// // List<Emplyoee> emplyoees=new List<Emplyoee>
+// // {
+// //     new Emplyoee{Name ="Amit",Salary=50000},
+// //     new Emplyoee{Name ="Ravi",Salary=60000},
+// //     new Emplyoee{Name ="Neha",Salary=70000},
+// // };
+
+    
+//     public static void Main(string[] args) 
+
+//  {  //int []number={1,2,3,4,5,6,7,8,9}; 
+
+
+// //  var evenNumber=number.Where(n=> n%2==0);
+
+// //  var numbers_a=number.Where(n => n > 3) . Select(n => n * 2) ; 
+
+// //  var ascending=number.OrderBy(n=>n);
+// //  var descending=number.OrderByDescending(n=>n);
+
+
+// //  Console.WriteLine("type of: "+numbers_a.GetType()); 
+// //  Console.WriteLine("Even Numbers are:");
+
+
+// // // ---------------------------------------------------
+// // Console.WriteLine("Printing the number in the descending order: ");
+// //   foreach(var n in descending) 
+// //   {  Console.WriteLine(n); 
+// //  } 
+
+// //  // ---------------------------------------------------
+
+// // Console.WriteLine("Printing the number in the Ascending order: ");
+// //   foreach(var n in ascending) 
+// //   {  Console.WriteLine(n); 
+// //  } 
+
+
+
+// // var SortedBySalary=Emplyoee.OrderBy(e=>e.Salary);
+
+// // foreach(var  S in SortedBySalary)
+// //         {
+// //             System.Console.WriteLine(s);
+// //         }
+
+
+
+
+
+
+
+//  }}
+
+//  //  class Student 
+//  // { string name;
+//  //  int marks; 
+//  // string grade; 
+//  // public static void Main(string[] args) 
+//  // { Student student_obj0=new Student(); 
+//  // Student student_obj1=new Student();
+//  //  Student student_obj2=new Student();
+//  //  Student student_obj3=new Student();
+//  //  Student student_obj4=new Student();
+//  //  var result = Student.Select(s => new { s.name, Grade=s.marks>60? "Pass":"Fail" });
+//  // } }
+
+
+
+
+
 using System;
-class Linq
+
+class ResourceHandler : IDisposable
 {
-    public static void Main(string[] args)
+    public ResourceHandler()
     {
-        
-        int []number={1,2,3,4,5,6,7,8,9};
-        
-        var evenNumber=number.Where(n=> n%2==0);
-       Console.WriteLine("type of: "+evenNumber.GetType());
+        Console.WriteLine("Resource acquired");
+    }
 
+    public void Dispose()
+    {
+        Console.WriteLine("Resource released");
+    }
 
-       System.Console.WriteLine("Even Numbers are:");
-
-       foreach(var n in evenNumber)
+    static void Main(string[] args)
+    {
+        using (ResourceHandler resourceHandler = new ResourceHandler())
         {
-            Console.WriteLine(n);
+            Console.WriteLine("Resource called...");
         }
-
+        
     }
 }
