646882f9b15e8bdcf4de51c1fd4fd8586d4655df
Shubhanshu <shubsriv001@gmail.com>
2026-01-09 23:10:28 +0530

    Added more task Code

--- CG_Day15/FileIo/Program.cs
+++ CG_Day15/FileIo/Program.cs
@@ -2,13 +2,40 @@
 using FileIO;
 using System.Security.AccessControl;
 using System.Text;
+using System.Diagnostics;
 class Program
 {
     public static void Main(string[] args)
     {
+    //     ----------------------Task 1 object----------------------
        UserTextManager obj=new UserTextManager();
        obj.WriteUserToFile("shubhanshu",21); 
        string data=obj.ReadUserFromFile();
        Console.WriteLine(data);
+
+
+
+    // ----------------------Task 2 object----------------------
+       UserBinaryManager Bobj=new UserBinaryManager();
+       Bobj.SaveUserBinary(101,"Bob");
+       
+       Console.WriteLine(Bobj.LoadUserBinary());
+
+    // ----------------------Task 3 object----------------------
+
+    FileSystemManager FMobj=new FileSystemManager();
+    Console.WriteLine("Enter Folder name");
+    string folder=Console.ReadLine();
+    Console.WriteLine("Enter File name");
+    string file=Console.ReadLine();
+
+    FMobj.CreateDirectory(folder);
+    FMobj.CreateFile(file);
+    Console.WriteLine(FMobj.CheckFileExists(file));
+    
+
+
+
+
     }
 }
--- CG_Day15/FileIo/TextHandling.cs
+++ CG_Day15/FileIo/TextHandling.cs
@@ -4,11 +4,15 @@ using System.Text;
 
 namespace FileIO
 {
+
+
+    // -------------Task 1---------------------------------------------------------------
     public class UserTextManager
     {
+        private string filePath="Userfile.txt";
         public void WriteUserToFile(string name, int age)
         {
-            using (StreamWriter writer = new StreamWriter("Userfile.txt"))
+            using (StreamWriter writer = new StreamWriter(filePath))
             {
                 writer.WriteLine("Name: " + name);
                 writer.WriteLine("Age: " + age);
@@ -20,9 +24,9 @@ namespace FileIO
             Console.WriteLine("User Data Loaded:");
             StringBuilder result = new StringBuilder();
 
-            using (StreamReader reader = new StreamReader("Userfile.txt"))
+            using (StreamReader reader = new StreamReader(filePath))
             {
-                string data;
+                string? data;
                 while ((data = reader.ReadLine()) != null)
                 {
                     result.AppendLine(data);
@@ -32,4 +36,76 @@ namespace FileIO
             return result.ToString();
         }
     }
+
+
+// -------------------Task 2----------------------------------------------------------------------------
+
+
+    public class UserBinaryManager
+    {
+        private string filePath="Userfile.bin";
+
+        public void SaveUserBinary(int id, string name)
+        {
+            using(BinaryWriter writer=new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                writer.Write(id);
+                writer.Write(name);
+            }
+        }
+
+
+        public string LoadUserBinary()
+        {
+                Console.WriteLine("User Loaded:");
+
+            using(BinaryReader reader=new BinaryReader(File.Open(filePath, FileMode.Open)))
+            {
+                int id=reader.ReadInt32();
+                string name=reader.ReadString();
+              return $"ID: {id}, Name: {name}";
+            }
+        }
+
+    }
+
+
+// -------------------Task 3----------------------------------------------------------------------------
+
+public class FileSystemManager{
+    private string baseDirectory;
+
+public void CreateDirectory(string folderName)
+        {
+            Directory.CreateDirectory(folderName);
+            if (Directory.Exists(folderName))
+            {
+                
+                Console.WriteLine("Directory Created");
+            }
+        
+        }
+
+    public void CreateFile(string fileName)
+        {
+            File.Create(fileName);
+            if (File.Exists(fileName))
+            {
+                
+            Console.WriteLine("File Created");
+            }
+           
+ 
+        }
+
+
+        public bool CheckFileExists(string fileName)
+        {
+                FileInfo fileInfo=new FileInfo(fileName);
+                
+            return fileInfo.Exists;
+            
+        }
+}
+
 }
