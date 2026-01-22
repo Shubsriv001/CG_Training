d182792b64815913af6391cec51ad6dc0c74a78a
Shubhanshu <shubsriv001@gmail.com>
2026-01-22 14:55:29 +0530

    code added

---
+++ SaturdayAssesment/jan24Assignment/ReflectConsole/ReflectConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using ReflectClass;
+
+class Program
+{
+    static void Main()
+    {
+        Assembly asm = Assembly.LoadFrom(@"C:\Users\Shubhanshu Srivastav\Desktop\C_SHARP\ReflectClass\ReflectClass\bin\Debug\net8.0\ReflectClass.dll");
+
+        foreach (Type type in asm.GetTypes())
+        {
+            Console.WriteLine(type.FullName);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            
+            int i = 1;
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine($"Method {i}: {method.Name}");
+                i++;
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
