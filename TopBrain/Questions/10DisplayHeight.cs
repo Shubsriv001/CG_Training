5fedd0a7a43aeb6f3d9033447e3347cc9b38aa33
Shubhanshu <shubsriv001@gmail.com>
2026-01-30 14:02:18 +0530

    Code added

---
+++ TopBrain/Questions/10DisplayHeight.cs
@@ -0,0 +1,28 @@
+using System;
+class DisplayHeight
+{
+
+    public void height_check()
+    {
+        Console.WriteLine("Enter the height in cm");
+        int height=Convert.ToInt32(Console.ReadLine());
+        if (height < 150)
+        {
+            Console.WriteLine("Short");
+        }
+        else if(height>=150 && height < 180)
+        {
+            Console.WriteLine("Average");
+        }
+        else if (height >= 180)
+        {
+            Console.WriteLine("Tall");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        DisplayHeight obj=new DisplayHeight();
+        obj.height_check();
+    }
+}
