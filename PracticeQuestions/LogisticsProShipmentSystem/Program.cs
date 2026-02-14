1af4cb4bf006e54a3f4da47b8c1298f52ab9c299
Shubhanshu <shubsriv001@gmail.com>
2026-02-14 14:58:02 +0530

    Logic Pro Shipment System Solution

---
+++ PracticeQuestions/LogisticsProShipmentSystem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    { 
+        ShipmentDetails obj=new ShipmentDetails();
+
+        Console.WriteLine("Enter the shipment code start with 'GC#'  and Ends with Digit");
+        string code_ship=Console.ReadLine();
+
+        Console.WriteLine("Enter the Transport Mode line Sea, Air, Land:");
+        string mode_ship=Console.ReadLine();
+
+        Console.WriteLine("Enter the Weight :");
+        double weight_ship=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the Storage Days:");
+        int storage_ship=Convert.ToInt32(Console.ReadLine());
+
+
+        obj.ShipmentCode=code_ship;
+        obj.TransportMode=mode_ship;
+        obj.Weight=weight_ship;
+        obj.StorageDays=storage_ship;
+
+        if(obj.ValidateShipmentCode())
+        {
+            Console.WriteLine("Valid Shipment Code");
+            Console.WriteLine("The total shipping cost is "+obj.CalculateTotalCost());
+        }
+        else
+        {
+            Console.WriteLine("Invalid Shipment Code");
+        }
+    }
+    
+}
---
+++ PracticeQuestions/LogisticsProShipmentSystem/Shipment.cs
@@ -0,0 +1,61 @@
+using System;
+class Shipment
+{
+    public string ShipmentCode{get;set;}
+    public string TransportMode{get;set;}
+    public double Weight{get;set;}
+    public int StorageDays{get;set;}
+}
+
+class ShipmentDetails:Shipment
+{
+    
+        
+    public bool ValidateShipmentCode()
+    {
+
+        string code="";
+        for(int i = 0; i < 3; i++)
+        {
+            code+=ShipmentCode[i];
+        }
+       
+       char c=ShipmentCode[3];
+        if(ShipmentCode.Length==7 && code=="GC#" && char.IsDigit(c))
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+        public double CalculateTotalCost()
+    {
+        if (TransportMode == "Sea")
+        {
+            
+        double TotalCost=(Weight*15.00)+Math.Sqrt(StorageDays);
+        double round=Math.Round(TotalCost,2);
+        return round;
+        }
+
+        else if (TransportMode == "Air")
+        {
+        
+        double TotalCost=(Weight*50.00)+Math.Sqrt(StorageDays);
+        double round=Math.Round(TotalCost,2);
+        return round;
+        }
+        else if (TransportMode == "Land")
+        {
+            
+        double TotalCost=(Weight*25.00)+Math.Sqrt(StorageDays);
+        double round=Math.Round(TotalCost,2);
+        return round;
+        }
+        return 0.0;
+
+    }
+
+
+    }
