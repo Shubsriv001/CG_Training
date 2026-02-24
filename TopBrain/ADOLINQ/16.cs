f14d74e630b0bf9e9a786b4654a4665b7560d4da
Shubhanshu <shubsriv001@gmail.com>
2026-02-24 12:10:56 +0530

    Solutions

---
+++ TopBrain/ADOLINQ/16.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+public class ProductDAL
+{
+    string connectionString = "Server=.\\SQLEXPRESS;Database=ADO;Integrated Security=True;TrustServerCertificate=True;";
+
+    public List<Product> GetAllProducts()
+    {
+        List<Product> products = new List<Product>();
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            string query = "SELECT ProductId, ProductName, Price FROM Product";
+
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                con.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product p = new Product();
+
+                        p.ProductId = Convert.ToInt32(reader["ProductId"]);
+                        p.ProductName = reader["ProductName"].ToString();
+                        p.Price = Convert.ToDecimal(reader["Price"]);
+
+                        products.Add(p);
+                    }
+                }
+            }
+        }
+
+        return products;
+    }
+}
---
+++ TopBrain/ADOLINQ/22.Linq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> data = new List<string>
+        {
+            "Ravi,87",
+            "Kumar,98",
+            "Arun,92"
+        };
+
+        var result = data.OrderByDescending(x => int.Parse(x.Split(',')[1])).Take(3).Select(x => x.Split(',')[0]).ToList();
+
+        result.ForEach(Console.WriteLine);
+    }
+}
